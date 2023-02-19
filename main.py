6b2cb4b08ec781b6c7bc593daa4696131c8e3640
abirukov2008 <abirukov2008@gmail.com>
2023-02-19 16:43:16 +0300

    fix types

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = FastAPI()
 @app.get("/weather/{city_name}")
 def weather_by_name(city_name: str):
     weather = Weather().get_by_city_name(city_name)
-    print(type(weather))
     weather_by_city = WeatherByCity.parse_raw(weather)
     return {
         "city": weather_by_city.name,
